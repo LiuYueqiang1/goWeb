2409d0b6f52614cd98df1296ac076b1733847a49
LiuYueqiang1 <122972454+LiuYueqiang1@users.noreply.github.com>
2023-06-16 21:04:40 +0800

    redis

---
+++ redix_demo/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+var rdb *redis.Client
+
+// 初始化连接
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:12379",
+		Password: "",  // no password set
+		DB:       0,   // use default DB
+		PoolSize: 100, // 连接池大小
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = rdb.Ping(ctx).Result()
+	return err
+}
+func main() {
+	if err := initClient(); err != nil {
+		fmt.Println("init redix client failed,err:", err)
+		return
+	}
+	fmt.Println("connect redix success!")
+	defer rdb.Close()
+}
